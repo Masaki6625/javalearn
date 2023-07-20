f38c2bce1d2b37b5aaa770e1a30c905fade54d2c
oku <yotubaki8171@gmail.com>
2023-07-20 13:38:06 +0000

    13章カプセル化途中

---
+++ java13/Hero.java
@@ -0,0 +1,30 @@
+public class Main {
+  //int hp;  これでは誰かが値を変更できてしまうなので
+  private int hp;  //とすると変更できなくなる
+  String name;
+  Sword sword;
+  
+  public void bye() {
+    System.out.println("勇者は別れを告げた");
+  }
+  
+  //ここもフィールドと同じ理由
+  private void die() {
+    System.out.println(this.name + "は死んでしまった!");
+    System.out.println("GAME OVERです。");
+  }
+  
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は眠って回復した！");
+  }
+  
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("お化けきのこ" + m.suffix + "から２ポイントの反撃を受けた！");
+    this.hp -= 2;
+    if (this.hp <= 0){
+     this.die();
+    }
+  }
+}
---
+++ java13/Inn.java
@@ -0,0 +1,7 @@
+//別の人が書いたコードがhp−１００する処理になっていた（意図しない処理）
+
+public class Inn {
+  public void checkIn(Hero h) {
+    h.hp = -100;
+  }
+}
